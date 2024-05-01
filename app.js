ae0ec7ab690eedb5b25850244a54ff21dc1a5ac6
chacha <chacha@sparcs.org>
2024-05-01 23:51:38 +0900

    Working on 2, 3... chacha

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const prisma = new PrismaClient();
 
 const problem1Router = require('./routes/problem-1');
 const problem2Router = require('./routes/problem-2');
+const problem3Router = require('./routes/problem-3');
+
 app.use('/problem', problem1Router);
 app.use('/problem', problem2Router);
+app.use('/problem', problem3Router);
 
 app.get('/', (req, res) => {
   res.send('Home');
