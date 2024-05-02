7bf102a3cb63e8581ae0dd00700998cc60b89b14
chacha <chacha@sparcs.org>
2024-05-03 03:33:02 +0900

    14 done

--- back/routes/problem-14.js
+++ back/routes/problem-14.js
@@ -6,7 +6,20 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 router.get('/14', async (req, res) => {
-  
+  const moscow = await prisma.employee.findMany({
+    where: {
+      Branch_Employee_branchNumberToBranch: {
+        branchName: 'Moscow'
+      }
+    }
+  });
+
+  const sum = moscow.reduce((inter, current) => {
+    const salary = current.salary;
+    return inter + salary;
+  }, 0);
+
+  res.send([{ 'sum of employees salaries': sum }]);
 });
 
 module.exports = router;
