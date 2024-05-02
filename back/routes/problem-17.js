c3e923fe814e290cbe6f0b25faa18d4891aae10f
chacha <chacha@sparcs.org>
2024-05-03 04:07:46 +0900

    prob 17 done

--- back/routes/problem-17.js
+++ back/routes/problem-17.js
@@ -6,6 +6,40 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 router.get('/17', async (req, res) => {
+  const customer = await prisma.customer.findMany({
+    include: {
+      Owns: {
+        include: {
+          Account: true
+        }
+      }
+    },
+    where: {
+      lastName: {
+        startsWith: 'S',
+        contains: 'e'
+      }
+    }
+  });
+  const filter = customer.filter(e => {
+    const accNum = new Set(e.Owns.flatMap(o => o.accNumber));
+    return accNum.size >= 3;
+  });
+
+  const result = filter.map(e => {
+      const total = e.Owns.reduce((sum, acc) => {
+        return sum + parseFloat(acc.Account.balance);
+      }, 0);
   
+      const average = total / e.Owns.length;
+      return {
+        customerID: e.customerID,
+        firstName: e.firstName,
+        lastName: e.lastName,
+        'average account balance': average 
+      };
+    });
+
+  res.send(result.sort((a, b) => a.customerID - b.customerID));
 });
 module.exports = router;
